8350bfcfb82c5f34bc866467e71fcfef7dac005e
twan-hub <adstith99@gmail.com>
2020-08-01 13:02:23 -0700

    Updated

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
@@ -5,6 +5,7 @@ import org.launchcode.javawebdevtechjobsauthentication.models.DTO.RegisterFormDT
 import org.launchcode.javawebdevtechjobsauthentication.models.User;
 import org.launchcode.javawebdevtechjobsauthentication.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Optional;
 
+@Controller
 public class AuthenticationController {
 
     @Autowired
